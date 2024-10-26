c7fddac94dc1a112f3ad5b41d7cd2fa33fbd6ea4
Martin <martinpugdal@live.dk>
2024-10-26 23:58:56 +0200

    Update app.vue

--- portfolio/app.vue
+++ portfolio/app.vue
@@ -5,3 +5,9 @@
     </NuxtLayout>
   </div>
 </template>
+<script setup lang="ts">
+useHead({
+  titleTemplate: '%s - Portfolio',
+  link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+})
+</script>
